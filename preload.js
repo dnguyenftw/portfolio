5ba8539a99ae1b1c21b3b9d2346d7d95b15b664d
Daniel Nguyen <danielsynguyen@gmail.com>
2019-07-27 11:30:21 -0700

    Lots of work done on Home page

---
+++ preload.js
@@ -0,0 +1,13 @@
+"use strict";
+(function() {
+
+  // set-up code that doesn't involve the DOM
+  //   (e.g. setting up initial values, arrays, etc.)
+
+
+
+  window.onload = function() {
+    // phew! your code goes here
+    
+  };
+})();
